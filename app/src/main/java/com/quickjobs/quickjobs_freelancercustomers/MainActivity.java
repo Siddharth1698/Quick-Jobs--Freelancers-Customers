64849fafcfa7ad40c1f03d0870280d7b9efd353f
Siddharth M <siddharth.m98@gmail.com>
2019-04-09 11:44:31 +0530

    Revert "Register back screen bug fixed"
    This reverts commit c6855cc3e38ae416639eb53df98386b2eefcbf79.

--- app/src/main/java/com/quickjobs/quickjobs_freelancercustomers/MainActivity.java
+++ app/src/main/java/com/quickjobs/quickjobs_freelancercustomers/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,FreelancerLoginActivity.class);
                 startActivity(intent);
+                finish();
                 return;
             }
         });
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,CustomerLoginActivity.class);
                 startActivity(intent);
+                finish();
                 return;
             }
         });
